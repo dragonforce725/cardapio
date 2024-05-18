3e5f6afa61b66c6c245eca893c1e6403ad0e4726
marcos <dragonforce725@gmail.com>
2024-05-17 23:54:40 -0300

    add mais produtos

--- script.js
+++ script.js
@@ -176,7 +176,7 @@ function checkRestaurantOpen() {
 
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 18 && hora <= 22;
+    return hora >= 18 && hora <= 23;
     
 }
 
